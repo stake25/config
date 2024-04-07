e90706c74c0776abb8f53c730c15947d2e408c9b
James Stakebake <stake@stake-lappy.home>
2024-04-07 16:53:08 -0400

    fix(zsh): fixed the zsh profile will be adding more to it later

--- install-apps.sh
+++ install-apps.sh
@@ -6,3 +6,13 @@ curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/lates
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm lazygit lazygit.tar.gz
+
+# install wezterm
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install packaged apps
+sudo apt install tldr
+sudo apt install plocate
+sodu apt install ranger
