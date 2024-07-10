cbaacaa9199e10349ee6def54b4cc62b4af65adc
James Stakebake <stake@pop-os.home>
2024-07-10 10:57:33 -0400

    re add the directory to get the config over properly

---
+++ neovim/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable swap files
+vim.opt.swapfile = false
+
+-- Set Colorcolumn
+vim.opt.colorcolumn = "100"
